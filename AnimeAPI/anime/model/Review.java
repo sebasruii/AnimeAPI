38041ee63df592cd0e7a90a3dd7e418a358b57f5
Francisco Benítez <frabenrui1@alum.us.es>
2022-05-10 13:09:15 +0200

    Estructura review

--- AnimeAPI/anime/model/Review.java
+++ AnimeAPI/anime/model/Review.java
@@ -1,10 +1,75 @@
 package anime.model;
 
-public class Review {
+import java.time.LocalDate;
 
-	public Object getId() {
-		// TODO Auto-generated method stub
-		return null;
+public class Review {
+	private String id;
+    private String idAnime;
+    private String comment;
+    private Integer rating;
+    private LocalDate date;
+
+    public Review() {}
+
+    public Review(String comment, Integer rating, LocalDate date) {
+        this.comment = comment;
+        this.rating = rating;
+        this.date = date;
+    }
+
+
+    public Review(String id, String comment, Integer rating, LocalDate date) {
+        this.id = id;
+        this.comment = comment;
+        this.rating = rating;
+        this.date = date;
+    }
+
+
+    public String getComment() {
+        return comment;
+    }
+
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+
+    public Integer getRating() {
+        return rating;
+    }
+
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+	public String getId() {
+		return id;
 	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getIdAnime() {
+		return idAnime;
+	}
+
+	public void setIdAnime(String idAnime) {
+		this.idAnime = idAnime;
+	}
+	
 
 }
