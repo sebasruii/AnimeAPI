c31868a1daa8e5a46a52858f83af5da6a6b6112a
JUAN CARLOS LOPEZ VEIGA <juanlopezveiga@gmail.com>
2022-05-28 13:40:06 +0200

    event get + delete

--- src/main/java/anime/api/resources/EventResource.java
+++ src/main/java/anime/api/resources/EventResource.java
@@ -4,16 +4,24 @@ import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.TemporalAdjusters;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
 
 import org.jboss.resteasy.spi.BadRequestException;
 import org.jboss.resteasy.spi.InternalServerErrorException;
+import org.jboss.resteasy.spi.NotFoundException;
 import org.restlet.data.ChallengeResponse;
 import org.restlet.data.ChallengeScheme;
 import org.restlet.resource.ClientResource;
@@ -76,6 +84,7 @@ public class EventResource {
 		Event event = new Event();
 		event.setEventName(user.getUserName());
 		
+		event.setDescription("New episode of: "+anime.getTitle());
 		LocalDate date = calcNext(anime.getBroadcast().getDayOfTheWeek());
 		
 		event.setDate(date.format(DateTimeFormatter.ISO_DATE));
@@ -95,4 +104,74 @@ public class EventResource {
 		DayOfWeek day = DayOfWeek.valueOf(dayOfWeek.toUpperCase());
 		return LocalDate.now().with(TemporalAdjusters.next(day));
 	}
+	
+	@GET
+    @Produces("application/json")
+    public Collection<Event> getEvents(@HeaderParam("token") String token){
+
+        User user = repository.getUserByToken(token);
+
+        if (user == null)
+            throw new BadRequestException("Incorrect token");
+
+        Event[] events = null;
+        List<Event> eventsUser = new ArrayList<Event>();
+        String nameUser = user.getUserName();
+
+        try {
+            ClientResource cr = new ClientResource(BASE_URI + "?eventName="+nameUser);
+
+            Token t = repository.getToken();
+
+            ChallengeResponse authHeader = new ChallengeResponse(new ChallengeScheme("JWT", "JWT"));
+            authHeader.setRawValue(t.getAccess());
+            cr.setChallengeResponse(authHeader);
+
+            events = cr.get(Event[].class);
+            for(Event e : events) {
+                eventsUser.add(e);
+            }
+
+        } catch (ResourceException re) {
+            throw new InternalServerErrorException("There was and error retrieving the events from EventsAPI");
+        }
+
+        if(eventsUser.isEmpty()) {
+            throw new NotFoundException("No events found for the user: " + nameUser);
+        }
+
+        return eventsUser;
+
+    }
+
+	@DELETE
+	@Path("/{eventId}")
+    public Response removeEvent(@HeaderParam("token") String token,@PathParam("eventId") String eventId ){
+
+        User user = repository.getUserByToken(token);
+        
+        if (user == null)
+            throw new BadRequestException("Incorrect token");
+
+        ClientResource cr = new ClientResource(BASE_URI+eventId+"/");
+        
+        Token t = repository.getToken();
+        
+    	ChallengeResponse authHeader = new ChallengeResponse(new ChallengeScheme("JWT", "JWT"));
+		authHeader.setRawValue(t.getAccess());
+
+		cr.setChallengeResponse(authHeader);
+		
+		
+        try {
+            cr.delete();
+
+        } catch (ResourceException re) {
+            System.err.println("Error when deleting the event: " + cr.getResponse().getStatus());
+            throw re;
+        }
+
+        return Response.noContent().build();
+
+    }
 }
